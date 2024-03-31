d429efa48e10a137ed526123b8d98d9128993b99
polinakelm <73700777+polinakelm@users.noreply.github.com>
2024-03-31 20:20:34 +0300

    add vue labs

---
+++ VueLabs/src/components/SpeedConverter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <label>
+            Скорость (км/ч):
+            <input type="number" v-model.number="speedKmph" @input="convertToMps">
+        </label>
+        <br>
+        <label>
+            Скорость (м/с):
+            <input type="number" v-model.number="speedMps" @input="convertToKmph">
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            speedKmph: 0,
+            speedMps: 0
+        }
+    },
+    methods: {
+        convertToMps() {
+            this.speedMps = (this.speedKmph / 3.6).toFixed(5);
+        },
+        convertToKmph() {
+            this.speedKmph = (this.speedMps * 3.6).toFixed(5);
+        }
+    }
+}
+</script>
