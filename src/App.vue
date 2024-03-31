20ceb67e8bd7ae796bd711a1b73b887c3bd253af
polinakelm <73700777+polinakelm@users.noreply.github.com>
2024-03-31 20:17:53 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <SpeedConverter/>
+    </div>
+</template>
+
+<script>
+import SpeedConverter from './components/SpeedConverter.vue'
+
+export default {
+    name: 'App',
+    components: {
+        SpeedConverter
+    }
+}
+</script>
